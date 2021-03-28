05551a3df5846a9c6fdd06798af3ab88c395b877
Anshika Jain <anshikajain604@gmail.com>
2021-03-29 02:14:48 +0530

    Add files via upload

---
+++ src/app/mapbox.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import * as Mapboxgl from 'mapbox-gl';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapboxService {
+
+  constructor() { }
+}
